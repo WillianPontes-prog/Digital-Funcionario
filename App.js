8ecf81c37b0e92edc00affa7f32f0b8672634eb0
Willian <Pichau@DESKTOP-8COHQ5A>
2025-05-18 16:41:13 -0300

    uiui tem senha e email agr, aiiiiiii, uiuiuiiiii

--- App.js
+++ App.js
@@ -1,16 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
 
 export default function App() {
 
+  const [loginUser, setLoginUser] = React.useState('');
+  const [passwordUser, setPasswordUser] = React.useState('');
+
   return (
+    
     <View style={styles.container}>
 
       <View style={styles.textLoginBox}>
-        <Text >Bem vindo!</Text>
-        
-        <Text>Faça login para continuar.</Text>
+        <Text style={{fontSize: 28, fontWeight: "bold"}}>
+        Bem vindo!</Text>
+
+        <Text style={{fontSize: 18}}>
+        Faça login para continuar.</Text>
+
+        <TextInput
+          style={[styles.input, {marginTop: 60}]}
+          onChangeText={setLoginUser}
+          value={loginUser}
+          placeholder="Login"
+          keyboardType="email-address"
+        />
+
+        <TextInput
+          style={[styles.input, {marginTop: -5}]}
+          onChangeText={setPasswordUser}
+          value={passwordUser}
+          placeholder="Senha"
+          keyboardType="password"
+          secureTextEntry={true}
+        />
+
       </View>
 
 
@@ -34,6 +58,14 @@ const styles = StyleSheet.create({
     justifyContent: 'top',
 
     marginTop: 150,
+  },
+
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 
 

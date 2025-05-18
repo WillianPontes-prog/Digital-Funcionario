2d8c3c4c11c06ae3f0bad5ec4625be06c736c162
Willian <Pichau@DESKTOP-8COHQ5A>
2025-05-18 16:10:35 -0300

    agr sim

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View style={styles.textLoginBox}>
+        <Text >Bem vindo!</Text>
+        
+        <Text>Faça login para continuar.</Text>
+      </View>
+
+
     </View>
   );
 }
@@ -17,4 +25,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textLoginBox: {
+
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'top',
+
+    marginTop: 150,
+  },
+
+
 });
